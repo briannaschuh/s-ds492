31d4c42c9462fc463a68600f4f19e23c8e61d25b
Brianna Schuh <70451400+briannaschuh@users.noreply.github.com>
2023-04-11 14:02:07 -0400

    Update main.py

--- feature-extraction/main.py
+++ feature-extraction/main.py
@@ -4,17 +4,17 @@ from feature_extraction import extract_features
 if __name__ == "__main__":
     l = len(sys.argv)
     
-    #Error checking
-    try: #Check to see if the first argument is valid
+    # error checking
+    try: # check to see if the first argument is valid
         inputtest = sys.argv[1]
         file = open('data/' + inputtest)
-    except IndexError: #raises if an argument was not given
+    except IndexError: # raises if an argument was not given
         print('The first argument states the name of the CSV file with the unhashed passwords')
         sys.exit(1)
-    except FileNotFoundError: #raises if the file does not exist
+    except FileNotFoundError: # raises if the file does not exist
         print("The file does not exist. ")
         sys.exit(1)
         
-    data = 'data/' + sys.argv[1] #location of the datasaet
+    data = 'data/' + sys.argv[1] # location of the datasaet
     
-    extract_features(data) #function that extracts the features
+    extract_features(data) # function that extracts the features
