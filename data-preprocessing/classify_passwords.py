9bc75aa0f277154623b2869ab09b461dd0086611
Brianna Schuh <70451400+briannaschuh@users.noreply.github.com>
2023-05-07 16:10:05 -0400

    Update classify_passwords.py

--- data-preprocessing/classify_passwords.py
+++ data-preprocessing/classify_passwords.py
@@ -1,56 +1,48 @@
-import re
-import csv
+import sys
+from feature_extraction import extract_features
+from sample_passwords import sample_passwords
+from classify_passwords import classify_passwords
 
-def classification(password):
-    score = 0
-    # rule 1: length
-    if len(password) < 8:
-        score += 1
-    elif len(password) <= 11:
-        score += 2
-    else:
-        score += 3
-    # rule 2: uppercase and lowercase letters
-    has_upper = bool(re.search(r'[A-Z]', password))
-    has_lower = bool(re.search(r'[a-z]', password))
-    if has_upper and has_lower:
-        score += 3
-    else:
-        score += 1
-    # rule 3: digits
-    num_digits = len(re.findall(r'\d', password))
-    if num_digits <= 1:
-        score += 1
-    else:
-        score += 2
-    # rule 4: special characters
-    num_special_chars = len(re.findall(r'[!#$%&\'()*+\-./:;<=>?@\[\]^_`{|}~¡]', password))
-    if num_special_chars <= 1:
-        score += 1
-    else:
-        score += 2
-    avg_score = score / 4 #calculate the average score
-
-    # determine password classification
-    if avg_score <= 1.5:
-        return "Weak"
-    elif avg_score <= 2.5:
-        return "Medium"
-    else:
-        return "Strong"
-
-def classify_passwords(data_filepath):
-    with open(data_filepath, newline='') as input_file:
-        reader = csv.reader(input_file) # create a csv reader
-        output = data_filepath[:-4] + "_classifed.csv" # name of the output file
-        with open(output, 'w', newline='') as output_file: # create the output file
-            writer = csv.writer(output_file)
-            writer.writerow(['password', 'strength']) # write the name of the headers
-        for row in reader: # iterate through each row
-            password = row[0] # extract the password
-            strength = classification(password) # apply the classification function
-            with open(output, 'a', newline='') as output_file: # open the output
-                writer = csv.writer(output_file)  # open writer
-                writer.writerow([password, strength]) # write the password and the strength
-    print("The passwords have been categorized. ")
-
+if __name__ == "__main__":
+    l = len(sys.argv)
+    
+    
+    # error checking
+    try: # check to see if the first argument is valid
+        inputtest = sys.argv[1]
+    except IndexError: # raises if an argument was not given
+        print("The first argument must specify the action. ")
+        sys.exit(1)
+    
+    try: # check to see if the second argument is valid
+        inputtest = sys.argv[2]
+        file = open('data/' + inputtest)
+    except IndexError: # raises if an argument was not given
+        print('The second argument states the name of the CSV file with the unhashed passwords')
+        sys.exit(1)
+    except FileNotFoundError: # raises if the file does not exist
+        print("The file does not exist. ")
+        sys.exit(1)
+        
+    command = sys.argv[1]
+    data = 'data/' + sys.argv[2] # location of the dataset
+    
+    # check to see if the command is valid
+    if command != 'extract' and command != 'subset' and command != 'classify':
+        print("Please use one of the valid commands: extract, subset, or classify")
+        sys.exit(1)
+    
+    # check to see if the third argument was given if the commad is subset
+    if command == 'subset':
+        try:
+            inputtest = sys.argv[3]
+        except IndexError:
+            print('The third argument for this command states the size of the subset')
+            sys.exit(1)
+    
+    if command == 'extract':
+        extract_features(data) # function that extracts the features
+    if command == 'subset':
+        sample_passwords(data, int(sys.argv[3])) # function that subsets the data
+    if command == 'classify':
+        classify_passwords(data) # fuction that classifies the password
