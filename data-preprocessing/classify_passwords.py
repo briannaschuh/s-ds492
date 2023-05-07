9c9e2d9c785ba7e8ef492685cd0f5e0bb6393056
Brianna Schuh <70451400+briannaschuh@users.noreply.github.com>
2023-05-07 16:10:52 -0400

    Update classify_passwords.py

--- data-preprocessing/classify_passwords.py
+++ data-preprocessing/classify_passwords.py
@@ -1,48 +1,37 @@
-import sys
-from feature_extraction import extract_features
-from sample_passwords import sample_passwords
-from classify_passwords import classify_passwords
+import csv
 
-if __name__ == "__main__":
-    l = len(sys.argv)
+import re
+import csv
+
+import zxcvbn
+
+def classification(password):
+    result = zxcvbn.zxcvbn(password)
+    score = result['score']
+
     
-    
-    # error checking
-    try: # check to see if the first argument is valid
-        inputtest = sys.argv[1]
-    except IndexError: # raises if an argument was not given
-        print("The first argument must specify the action. ")
-        sys.exit(1)
-    
-    try: # check to see if the second argument is valid
-        inputtest = sys.argv[2]
-        file = open('data/' + inputtest)
-    except IndexError: # raises if an argument was not given
-        print('The second argument states the name of the CSV file with the unhashed passwords')
-        sys.exit(1)
-    except FileNotFoundError: # raises if the file does not exist
-        print("The file does not exist. ")
-        sys.exit(1)
-        
-    command = sys.argv[1]
-    data = 'data/' + sys.argv[2] # location of the dataset
-    
-    # check to see if the command is valid
-    if command != 'extract' and command != 'subset' and command != 'classify':
-        print("Please use one of the valid commands: extract, subset, or classify")
-        sys.exit(1)
-    
-    # check to see if the third argument was given if the commad is subset
-    if command == 'subset':
-        try:
-            inputtest = sys.argv[3]
-        except IndexError:
-            print('The third argument for this command states the size of the subset')
-            sys.exit(1)
-    
-    if command == 'extract':
-        extract_features(data) # function that extracts the features
-    if command == 'subset':
-        sample_passwords(data, int(sys.argv[3])) # function that subsets the data
-    if command == 'classify':
-        classify_passwords(data) # fuction that classifies the password
+    if score == 0 or score == 1:
+        return "Weak"
+    elif score == 2:
+        return "Medium"
+    elif score == 3:
+        return "Strong"
+    elif score == 4:
+        return "Very Strong"
+
+
+def classify_passwords(data_filepath):
+    with open(data_filepath, newline='') as input_file:
+        reader = csv.reader(input_file) # create a csv reader
+        output = data_filepath[:-4] + "_classifed.csv" # name of the output file
+        with open(output, 'w', newline='') as output_file: # create the output file
+            writer = csv.writer(output_file)
+            writer.writerow(['password', 'strength']) # write the name of the headers
+        for row in reader: # iterate through each row
+            password = row[0] # extract the password
+            strength = classification(password) # apply the classification function
+            with open(output, 'a', newline='') as output_file: # open the output
+                writer = csv.writer(output_file)  # open writer
+                writer.writerow([password, strength]) # write the password and the strength
+    print("The passwords have been categorized. ")
+
